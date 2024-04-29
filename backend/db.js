9380d48b51a7d314e099ebc5f6a0c69cf9dc0f16
preeetham31 <spreethamkumar5@gmail.com>
2024-04-29 10:23:09 +0530

    updated mongoose cred

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://preetham3113:Preethampipi1331@cluster0.5tgpm3j.mongodb.net/")
+mongoose.connect("mongodb+srv://preetham1331:Preethampipi1331@cluster0.5tgpm3j.mongodb.net/")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
