98920b2ac1bf75f93ebcf88dcb4ffc7cd5f23ca8
preeetham31 <spreethamkumar5@gmail.com>
2024-04-29 09:28:08 +0530

    writing backend

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://preetham3113:Preethampipi1331@cluster0.5tgpm3j.mongodb.net/")
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo
+}
